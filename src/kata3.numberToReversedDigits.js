4bc373593522e9107b073e375dc8cbd494691c69
Irfan Hussain <irfan185@gmail.com>
2022-06-08 19:22:56 +0100

    3rd kata complete

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,9 @@
-const numberToReversedDigits = number => {};
+const numberToReversedDigits = (number) => {
+  //string method converts the number to a string, use Number to convert each string into a number
+  const digits = Array.from(String(number), Number);
+  digits.reverse();
+  console.log(digits);
+  return digits;
+};
 
 module.exports = numberToReversedDigits;
