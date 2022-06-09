8bae5e51f96461f8fc815a8ad3248dda1a5486a4
Irfan Hussain <irfan185@gmail.com>
2022-06-09 23:42:51 +0100

    cat dog years kata complete

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,14 @@
-const humanCatDogYears = number => {};
+const humanCatDogYears = (number) => {
+  if (number < 0) {
+    return "error";
+  }
+  const catYears = 24 + (number - 2) * 4;
+  const dogYears = 24 + (number - 2) * 5;
+  const resultsArr = [];
+  resultsArr.push(number, catYears, dogYears);
+  return resultsArr;
+};
+
+console.log(humanCatDogYears(38));
 
 module.exports = humanCatDogYears;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,10 @@
 const { humanCatDogYears } = require("../src");
 
-// Look Ma, no handlebars!!!
+describe("humanCatDogYears", () => {
+  it("returns an array of three numbers", () => {
+    expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
+    expect(humanCatDogYears(25)).toEqual([25, 116, 139]);
+    expect(humanCatDogYears(60)).toEqual([60, 256, 314]);
+    expect(humanCatDogYears(-23)).toEqual("error");
+  });
+});
