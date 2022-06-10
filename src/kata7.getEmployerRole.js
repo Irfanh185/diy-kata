25015d3b9caa050c75a2e27a8dcaaab29642c116
Irfan Hussain <irfan185@gmail.com>
2022-06-10 19:13:05 +0100

    kata7 complete

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,3 +1,28 @@
-const getEmployerRole = (employeeName, employees) => {};
+const getEmployerRole = (employeeName, employees) => {
+  for (let i = 0; i < employees.length; i++) {
+    if (employees[i].name === employeeName) {
+      return employees[i].role;
+    }
+  }
+};
+const employees = [
+  {
+    name: "Satti",
+
+    role: "Developer",
+  },
+  {
+    name: "Jenny",
+
+    role: "Sales Associate",
+  },
+  {
+    name: "Steve",
+
+    role: "Footballer",
+  },
+];
+
+console.log(getEmployerRole("Steve", employees));
 
 module.exports = getEmployerRole;
