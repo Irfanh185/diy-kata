9af409dc941afe9f5d5185803a323e883e17b897
Irfan Hussain <irfan185@gmail.com>
2022-06-10 18:32:50 +0100

    kata6 complete

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,43 @@
-const joinNames = namesObj => {};
+const joinNames = (namesObj) => {
+  const namesArr = [];
+  for (let i = 0; i < namesObj.length; i++) {
+    namesArr.push(namesObj[i].name);
+  }
+
+  if (namesArr.length === 3) {
+    return `${namesArr[0]}, ${namesArr[1]} & ${namesArr[2]}`;
+  } else if (namesArr.length === 4) {
+    return `${namesArr[0]}, ${namesArr[1]}, ${namesArr[2]} & ${namesArr[3]}`;
+  } else if (namesArr.length === 5) {
+    return `${namesArr[0]}, ${namesArr[1]}, ${namesArr[2]}, ${namesArr[3]} & ${
+      namesArr[4]
+    }`;
+  }
+};
+
+const newNames = [
+  {
+    name: "Bart",
+  },
+  {
+    name: "Lisa",
+  },
+  {
+    name: "Maggie",
+  },
+  {
+    name: "Irfan",
+  },
+  {
+    name: "Hina",
+  },
+  {
+    name: "Haris",
+  },
+];
 
 module.exports = joinNames;
+
+// //using a for of loop
+// for (let indexValue of arr.entries()) {
+//   console.log(indexValue)
--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,51 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  test("returns string of names, seperated by commas and an ampersand", () => {
+    const result1 = [
+      {
+        name: "Bart",
+      },
+      {
+        name: "Lisa",
+      },
+      {
+        name: "Maggie",
+      },
+    ];
+    const result2 = [
+      {
+        name: "Bart",
+      },
+      {
+        name: "Lisa",
+      },
+      {
+        name: "Maggie",
+      },
+      {
+        name: "Irfan",
+      },
+    ];
+    const result3 = [
+      {
+        name: "Bart",
+      },
+      {
+        name: "Lisa",
+      },
+      {
+        name: "Maggie",
+      },
+      {
+        name: "Irfan",
+      },
+      {
+        name: "Hina",
+      },
+    ];
+    expect(joinNames(result1)).toEqual("Bart, Lisa & Maggie");
+    expect(joinNames(result2)).toEqual("Bart, Lisa, Maggie & Irfan");
+    expect(joinNames(result3)).toEqual("Bart, Lisa, Maggie, Irfan & Hina");
+  });
 });
