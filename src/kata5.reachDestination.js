b92229d123869cdfdc9389526c8d758c2fb72aa8
Irfan Hussain <irfan185@gmail.com>
2022-06-10 12:46:25 +0100

    kata 5 complete

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,8 @@
-const reachDestination = (distance, speed) => {};
-
+const reachDestination = (distance, speed) => {
+  const destinationTime = distance / speed;
+  const result = Math.round(destinationTime / 0.5) * 0.5;
+  return `I should be there in ${result} hours.`;
+};
+console.log(Math.round(4.4 / 0.5) * 0.5);
+console.log(reachDestination(80, 56));
 module.exports = reachDestination;
